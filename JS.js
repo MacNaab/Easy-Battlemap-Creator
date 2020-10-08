829d49e7bfda4add27bfbd93005bf016e5febea5
MacNaab <61412526+MacNaab@users.noreply.github.com>
2020-10-08 23:39:37 +0200

    Update JS.js

--- JS.js
+++ JS.js
@@ -94,11 +94,11 @@ function previewImage() {
 
         fileReader.onload = function (event) {
             $("#BMT17").append( '<img class="IMAGE_AFF" src='+event.target.result+'>' );
+		image_change();
         };
 
         fileReader.readAsDataURL(file[i]);
         }
-	image_change();
     }
 }
 
